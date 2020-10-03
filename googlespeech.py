f803941e06960721f3f0b3930d69f8da9825db3b
rv1234567 <44345447+rv1234567@users.noreply.github.com>
2020-10-03 12:58:18 +0530

    Update googlespeech.py
    comments required to explain code

--- googlespeech.py
+++ googlespeech.py
@@ -1,16 +1,16 @@
-import speech_recognition as sr
+import speech_recognition as sr //google library to read voice
 import webbrowser as wb
-chrome_path='google-chrome'
+chrome_path='google-chrome'         //read googlechrome patth to search voice command
 r=sr.Recognizer()
-with sr.Microphone() as source:
+with sr.Microphone() as source:         //listens to microphones
     print("say something")
     audio=r.listen(source)
     print("done")
 
 try:
-    text=r.recognize_google(audio)
+    text=r.recognize_google(audio)                  //converting audio to text
     print("rishabh thinks yu said:\n"+text)
     wb.get(chrome_path).open(text)
 
-except Exception as e:
+except Exception as e:                                //prints the output
     print(e)
